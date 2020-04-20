bbcf064bc76606b483d1c0610241eb4947e19ceb
s5202188 <s5202188@bournemouth.ac.uk>
2020-04-20 18:12:27 +0100

    2nd

--- logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
@@ -7,16 +7,17 @@ import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.MutableSparseMatrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.SparseMatrix
 import kotlin.random.Random
 
-class StudentDotsBoxGame : AbstractDotsAndBoxesGame() {
-    override val players: List<Player> = TODO("You will need to get players from your constructor")
+class StudentDotsBoxGame(columns: Int, rows: Int, players: List<Player>) : AbstractDotsAndBoxesGame() {
+
+    override val players: List<Player> = players    //TODO("You will need to get players from your constructor")
 
     override val currentPlayer: Player get()= TODO("Determine the current player, like keeping" +
                                                            "the index into the players list")
 
     // NOTE: you may want to me more specific in the box type if you use that type in your class
-    override val boxes: Matrix<DotsAndBoxesGame.Box> = TODO("Create a matrix initialized with your own box type")
+    override val boxes: Matrix<DotsAndBoxesGame.Box> = MutableMatrix(columns, rows, ::StudentBox)  //TODO("Create a matrix initialized with your own box type")
 
-    override val lines: SparseMatrix<DotsAndBoxesGame.Line> = TODO("Create a matrix initialized with your own line type")
+    override val lines: SparseMatrix<DotsAndBoxesGame.Line> = MutableSparseMatrix(columns, rows, ::StudentLine)  //TODO("Create a matrix initialized with your own line type")
 
     override val isFinished: Boolean
         get() = TODO("Provide this getter. Note you can make it a var to do so")
--- logic/src/test/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGameTest.kt
+++ logic/src/test/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGameTest.kt
@@ -10,7 +10,7 @@ import uk.ac.bournemouth.ap.dotsandboxeslib.test.TestDotsAndBoxes
  */
 internal class StudentDotsBoxGameTest : TestDotsAndBoxes() {
     override fun createGame(columns: Int, rows: Int, players: List<Player>): DotsAndBoxesGame {
-        //return StudentDotsBoxGame(columns, rows, players)
-        return TODO("Create a game of your game type with the correct columns, rows and players")
+        return StudentDotsBoxGame(columns, rows, players)
+        //return TODO("Create a game of your game type with the correct columns, rows and players")
     }
 }
